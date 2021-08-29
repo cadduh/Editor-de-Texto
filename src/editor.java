68ecc49a8d5535b002b3c41cd4acc3a125fa256e
Carlos-edward <carlos.eduardo.brito.dev@gmail.com>
2021-08-29 16:40:26 -0300

    Estilo de Fonte adicionado

--- src/editor.java
+++ src/editor.java
@@ -13,13 +13,16 @@ public class editor extends JFrame {
 	private JLabel img;
 	private JScrollPane scrollDoTexto;
 	private JMenuBar menuBarra; // Barra de menu
-	private JMenu fontes,Cores ;
-	private JMenuItem Arial,timeNewRoman,Georgia,tamanho;
+	private JMenu fontes,Cores,EstilosdeFonte ;
+	private JMenuItem Arial,timeNewRoman,Georgia,tamanho,Normal,Bold,Italic,BoldItalic;
 	private JMenuItem Vermelho,Azul,Amarelo,Rosa,Roxo,Verde;
 	private JMenuItem calibre[] = new JMenuItem[30];
 	
 	private JTextArea textAreaEditor;
 	private String font = "";
+	private int tamanhoDaLetra = 15;
+	private int estiloDaLetra = 0;
+	
         
     
 
@@ -63,6 +66,7 @@ public class editor extends JFrame {
 		//Tamanho
 		
 		tamanho = new JMenu("Tamanho   |");
+		tamanho.setFont(new Font("Impact",5,15));
 		
 		for (int i=0; i < 30;i++) {
 			calibre[i] = new JMenuItem(String.valueOf(i));			
@@ -74,7 +78,9 @@ public class editor extends JFrame {
 		
 		//COR 
 		 	
-		Cores = new JMenu("Cores");
+		Cores = new JMenu("Cores  |");
+		Cores.setFont(new Font("Impact",5,15));
+		
 		Amarelo = new JMenuItem("Amarelo");
 		Vermelho = new JMenuItem("Vermelho");
 		Verde = new JMenuItem("Verde");
@@ -89,6 +95,26 @@ public class editor extends JFrame {
 		menuBarra.add(Cores);
 	
 	    setJMenuBar(menuBarra);
+	    
+	    //FONT STYLES
+	    EstilosdeFonte = new JMenu("Estilos de Fonte  |");
+	    EstilosdeFonte.setFont(new Font("Impact",5,15));
+	    // Normal,Bold,Italic,Bold Italic;
+	    
+	    Normal = new JMenuItem("Normal");
+	    Bold = new JMenuItem("Bold");
+	    Italic = new JMenuItem("Italic");
+	    BoldItalic = new JMenuItem("Bold Italic");
+	    
+	    EstilosdeFonte.add(Normal);
+	    EstilosdeFonte.add(Bold);
+	    EstilosdeFonte.add(Italic);
+	    EstilosdeFonte.add(BoldItalic);
+	    menuBarra.add(EstilosdeFonte);
+	    
+	    
+	    
+	    
 		
 	}
 	
@@ -98,8 +124,9 @@ public class editor extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				textAreaEditor.setFont(new Font("Arial",0,15));
+				textAreaEditor.setFont(new Font("Arial",estiloDaLetra,tamanhoDaLetra));
 				fontes.setText("Arial      |");
+				font = "Arial";
 				
 			}
 		});
@@ -108,8 +135,9 @@ public class editor extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				textAreaEditor.setFont(new Font("Georgia",0,15));
+				textAreaEditor.setFont(new Font("Georgia",estiloDaLetra,tamanhoDaLetra));
 				fontes.setText("Georgia      |");
+				font = "Georgia";
 				
 			}
 		});
@@ -119,8 +147,8 @@ public class editor extends JFrame {
 			calibre[i].addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					// TODO Auto-generated method stub
-					textAreaEditor.setFont(new Font("Arial",2,num));
-					
+					textAreaEditor.setFont(new Font(font,estiloDaLetra,num));
+					tamanhoDaLetra = num;
 				}
 			});
 	   }
@@ -166,10 +194,48 @@ public class editor extends JFrame {
 				textAreaEditor.setForeground(new Color(153,51,153));
 			}
 		});
+		
+		Normal.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				textAreaEditor.setFont(new Font(font,0,tamanhoDaLetra));
+				estiloDaLetra = 0;
+				
+			}
+		});
+		
+		Bold.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				textAreaEditor.setFont(new Font(font,1,tamanhoDaLetra));
+				estiloDaLetra = 1;
+				
+			}
+		});
+		
+		Italic.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				textAreaEditor.setFont(new Font(font,2,tamanhoDaLetra));
+				estiloDaLetra = 2;
+				
+			}
+		});
+		
+		BoldItalic.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				textAreaEditor.setFont(new Font(font,3,tamanhoDaLetra));
+				estiloDaLetra = 3;
+				
+			}
+		});
+		
 	}
 	
 	public static void main(String[] args) {
 		editor frame = new editor();
+		frame.setTitle("Editor de texto");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setBounds(0,0,1280,720);
 	    frame.setVisible(true);
